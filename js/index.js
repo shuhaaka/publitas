5c493ee80731b4f57b29ca5e8f3fee15e74b1f6e
Shuhrat <shuhaaka@mail.ru>
2019-12-28 23:18:35 +0500

    Publication JS

--- js/index.js
+++ js/index.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
         $(this).parents(".css-1yv5pu4").find(".css-jj2p7w").show();
     });
 });
+$(document).ready(function() {
+    $(".css-1awgttx ").click(function() {
+        $(this).removeClass("css-1awgttx ").addClass("css-n8yzpt");
+        $(this).parents(".css-w0yocm").find(".css-jj2p7w").show();
+    });
+});
 //Close function of Zayavka Button
 jQuery(function($) {
     $(document).mouseup(function(e) { // событие клика по веб-документу
@@ -22,3 +28,22 @@ jQuery(function($) {
         }
     });
 });
+$(".css-82yxlt").click(function(){
+    $(".css-137s5mo").show();
+})
+
+$(".css-137s5mo .css-1a100vq").click(function(){
+    $(".css-137s5mo").hide();
+});
+
+(function($) {
+    $(function() {
+     
+      $('.css-v1nb4p').on('click', '.css-1jk11g9:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('.css-du3tfb').find('.css-i9c7c').removeClass('active').eq($(this).index()).addClass('active');
+      });
+     
+    });
+})(jQuery);
